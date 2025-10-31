088b0aeac2dfbaf8b6ece991d26c8b17fba84b39
yaoya <cxx722@163.com>
2025-10-31 20:37:33 +0800

    Systematic debugging has been completed.

--- app/grpc_api/generated/user_pb2_grpc.py
+++ app/grpc_api/generated/user_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import app.grpc_api.generated.user_pb2 as user__pb2
+from . import user_pb2 as user__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
